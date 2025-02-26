ccd788b5c87511775d1c03fd41e3d61a12d6d465
Thisara Dilshan <it21067938@my.sliit.lk>
2025-02-27 02:01:54 +0530

    top deals design

--- frontend/src/pages/deals/Deals.tsx
+++ frontend/src/pages/deals/Deals.tsx
@@ -1,23 +1,54 @@
 import React from "react";
 import Breadcrumbs from "../../components/breadcrumbs/BreadCrumbs";
+import assets from "./../../assets/img/assets";
+import { Clock, BadgeCheck  } from "lucide-react";
 
 function Deals() {
   return (
-    <div>
+    <>
       <Breadcrumbs />
       <div className="deals">
-        <div className="deals-category">
-          <select className="deals-dropdown">
-            <option value="all">All</option>
-            <option value="electronics">Electronics</option>
-            <option value="home&kitchen">Home & Kitchen</option>
-            <option value="sports">Sports</option>
-          </select>
-          <p>Top Brands</p>
-          <p>Weekly Deals</p>
+        <div className="deals-header">
+          <h1 className="deals-heading">🔥 Today's Top Deals</h1>
+          <br />
+          <div className="deals-category">
+            <select className="deals-dropdown">
+              <option value="all">All Categories</option>
+              <option value="electronics">Electronics</option>
+              <option value="home&kitchen">Home & Kitchen</option>
+              <option value="sports">Sports</option>
+            </select>
+            <div className="deals-links">
+              <p>Top Brands</p>
+              <p>Weekly Deals</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="deals-items-section">
+          <div className="deals-items">
+            {[...Array(15)].map((_, index) => (
+              <div className="deals-card" key={index}>
+                <div className="ribbon">
+                  <Clock className="clock" size={15} strokeWidth={2} />
+                </div>
+                <img src={assets.logo} alt="Product" />
+                <div className="deals-card-content">
+                  <h2>Apple iPhone 13 128GB Unlocked - Excellent</h2>
+                  <h1>$324.99</h1>
+                  <br />
+                  <div className="deals-card-badge">
+                    <BadgeCheck size={13} strokeWidth={3} color="green"/>
+                  <p>Prime Picks Refurbished</p>
+                  </div>
+                  
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
